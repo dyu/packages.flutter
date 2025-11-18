c444dfa0f8b158002039ee6a32fb3d081657728e
dyu <david.yu.ftw@gmail.com>
2025-11-18 09:27:32 +0800

    migrate deprecated subclass method for flutter 3.x

--- packages/pdfx/lib/src/viewer/pdf_page_image_provider.dart
+++ packages/pdfx/lib/src/viewer/pdf_page_image_provider.dart
@@ -1,6 +1,5 @@
-// ignore: unnecessary_import
-import 'dart:typed_data';
-import 'dart:ui' as ui show Codec;
+import 'dart:ui';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pdfx/src/renderer/interfaces/page.dart';
@@ -20,10 +19,12 @@ class PdfPageImageProvider extends ImageProvider<PdfPageImageProvider> {
   final double scale;
 
   @override
-  // ignore: deprecated_member_use
-  ImageStreamCompleter load(PdfPageImageProvider key, DecoderCallback decode) =>
+  ImageStreamCompleter loadImage(
+    PdfPageImageProvider key,
+    ImageDecoderCallback decode,
+  ) =>
       MultiFrameImageStreamCompleter(
-        codec: _loadAsync(key, decode),
+        codec: _loadAsync(key, decode), 
         scale: key.scale,
         informationCollector: () sync* {
           yield ErrorDescription('Page: $pageNumber, DocumentId: $documentId');
@@ -34,21 +35,23 @@ class PdfPageImageProvider extends ImageProvider<PdfPageImageProvider> {
   Future<PdfPageImageProvider> obtainKey(ImageConfiguration configuration) =>
       SynchronousFuture<PdfPageImageProvider>(this);
 
-  Future<ui.Codec> _loadAsync(
-      PdfPageImageProvider key,
-      // ignore: deprecated_member_use
-      DecoderCallback decode) async {
+  Future<Codec> _loadAsync(
+    PdfPageImageProvider key,
+    ImageDecoderCallback decode,
+  ) async {
     assert(key == this);
 
     final loadedPdfPageImage = await pdfPageImage;
-    final Uint8List bytes = loadedPdfPageImage.bytes;
+    final bytes = loadedPdfPageImage.bytes;
 
     if (bytes.lengthInBytes == 0) {
       throw StateError('${loadedPdfPageImage.pageNumber} page '
           'cannot be loaded as an image.');
     }
 
-    return decode(bytes);
+    final buffer = await ImmutableBuffer.fromUint8List(bytes);
+
+    return decode(buffer);
   }
 
   @override
